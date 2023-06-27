19ab19775e627cfbea18b187340b89d22e72b820
Taylor <taylor@Taylors-MacBook-Pro.local>
2023-06-27 18:25:57 +0100

    feat: added settings to enable/disable repeat UUID

--- main.ts
+++ main.ts
@@ -1,49 +1,109 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
-import { v4 as uuidv4 } from 'uuid';
+import {
+	App,
+	Command,
+	Editor,
+	MarkdownFileInfo,
+	MarkdownView,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import { v4 as uuidv4 } from "uuid";
 
 interface UUIDGeneratorPluginSettings {
-	mySetting: string;
+	enableRepeat: boolean;
 }
 
 const DEFAULT_SETTINGS: UUIDGeneratorPluginSettings = {
-	mySetting: 'default'
-}
+	enableRepeat: false,
+};
 
 export default class UUIDGenerator extends Plugin {
 	settings: UUIDGeneratorPluginSettings;
-
+	repeatCommand: Command;
 	lastUUID: string;
 
 	async onload() {
 		await this.loadSettings();
 
 		this.addCommand({
-			id: 'generate-uuid-v4',
-			name: 'Generate UUID at Cursor',
+			id: "generate-uuid-v4",
+			name: "Generate UUID at Cursor",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				this.lastUUID = uuidv4();
 				editor.replaceSelection(this.lastUUID);
-			}
+			},
 		});
 
 		this.addCommand({
-		id: 'repeat-uuid-v4',
-			name: 'Repeat UUID at Cursor',
+			id: "repeat-uuid-v4",
+			name: "Repeat UUID at Cursor",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				editor.replaceSelection(this.lastUUID);
-			}
+			},
+			editorCheckCallback: (
+				checking: boolean,
+				editor: Editor,
+				ctx: MarkdownView | MarkdownFileInfo
+			): boolean | void => {
+				if (checking) {
+					return (
+						this.settings.enableRepeat &&
+						this.lastUUID !== undefined
+					);
+				} else if (this.lastUUID == undefined) {
+					return false;
+				}
+
+				editor.replaceSelection(this.lastUUID);
+			},
 		});
+
+		this.addSettingTab(new UUIDGeneratorSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+}
+
+class UUIDGeneratorSettingTab extends PluginSettingTab {
+	plugin: UUIDGenerator;
+
+	constructor(app: App, plugin: UUIDGenerator) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "UUID Generator Settings" });
+
+		new Setting(containerEl)
+			.setName("Enable Repeat UUID at Cursor")
+			.setDesc(
+				"Enables/disables the 'Repeat UUID at Cursor' command to prevent confusion"
+			)
+			.addToggle((Boolean) =>
+				Boolean.setValue(this.plugin.settings.enableRepeat).onChange(
+					async (value) => {
+						this.plugin.settings.enableRepeat = value;
+						await this.plugin.saveSettings();
+					}
+				)
+			);
 	}
 }
